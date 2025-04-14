0d7d61e40fd99464d3d639330ed3764524f3c695
lazul1ne <78873973+lazul1ne@users.noreply.github.com>
2025-04-14 17:09:30 -0400

    upload template

---
+++ src/client/java/xyz/lazuline/UltimateOriginsUltilityModClient.java
@@ -0,0 +1,10 @@
+package xyz.lazuline;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateOriginsUltilityModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/xyz/lazuline/UltimateOriginsUltilityModDataGenerator.java
@@ -0,0 +1,11 @@
+package xyz.lazuline;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UltimateOriginsUltilityModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
